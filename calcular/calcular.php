830f44587a242ea27f797caa388b818b38b0a301
João Pedro de Araújo <79383821+Joaoaraujo61@users.noreply.github.com>
2026-06-25 16:47:14 -0300

    Add files via upload

---
+++ calcular/calcular.php
@@ -0,0 +1,9 @@
+<?php
+
+$numero1 = $_POST["primeiroNumero"];
+$numero2 = $_POST["segundoNumero"];
+$multiplicacao = $numero1*$numero2;
+
+echo("A multiplicação entre $numero1 e $numero2 é $multiplicacao")
+
+?>
